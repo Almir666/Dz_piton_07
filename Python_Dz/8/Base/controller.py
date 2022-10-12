4828e15f63339d9aa6dbeb33a7fe287f87b508d0
Almir <crafteasy@yandex.ru>
2022-10-12 14:13:35 +0300

    Начинаю делать крестики нолики

--- Python_Dz/8/Base/controller.py
+++
@@ -1,5 +0,0 @@
-import model_data as md 
-from view import Value
-
-
-md.write_data(Value())
--- Python_Dz/8/Base/model_data.py
+++
@@ -1,8 +0,0 @@
-# value = input('Введите имя, телефон, пол(м\ж), возраст, должность, з\п сотрудника через пробел: ')
-
-def write_data(value):
-    with open('data.txt', 'a') as data:
-        for i in value:
-            data.write(''.join(i))
-        data.write('\n')
-# write_data(value)          
